fd8e8583f94bd8c105e799d74c9bb5fc8cd239a8
VitorMM <vitor251093@gmail.com>
2024-01-04 18:08:29 -0300

    Adding recap endpoint to get logs

--- server/src/service/ReCapService.cs
+++ server/src/service/ReCapService.cs
@@ -10,6 +10,14 @@ namespace HttpServer
 {
     class ReCapService
     {
+        [ApiMethod(Name="api.game.log")]
+        public static byte[] log(NameValueCollection parameters)
+        {
+            string message = parameters.Get("message");
+            Console.WriteLine(message);
+            return new byte[]{};
+        }
+
         [ApiMethod(Name="api.game.registration")]
         public static byte[] registerUser(NameValueCollection parameters)
         {
