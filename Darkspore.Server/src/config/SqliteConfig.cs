15f0d22c1022f37b1d4c93263c9da1fcbb722186
VitorMM <vitor251093@gmail.com>
2024-05-14 19:00:31 -0300

    Adding default creature parts

--- Darkspore.Server/src/config/SqliteConfig.cs
+++ Darkspore.Server/src/config/SqliteConfig.cs
@@ -8,6 +8,7 @@ public class SqliteConfig : DbContext
 {
     public DbSet<AccountModel> Accounts { get; set; }
     public DbSet<CreatureTemplateModel> CreatureTemplates { get; set; }
+    public DbSet<CreaturePartModel> CreatureParts { get; set; }
 
     private string DbPath;
 
@@ -27,6 +28,12 @@ public class SqliteConfig : DbContext
             var templatesStr = File.ReadAllText("./resources/templates.json");
             var templates = JsonSerializer.Deserialize<List<CreatureTemplateModel>>(templatesStr);
             this.CreatureTemplates.AddRange(templates);
+            this.SaveChanges();
+        }
+        if (this.CreatureParts.SingleOrDefault(b => b.rigblockAssetId == 1) == null) {
+            var partsStr = File.ReadAllText("./resources/parts.json");
+            var parts = JsonSerializer.Deserialize<List<CreaturePartModel>>(partsStr);
+            this.CreatureParts.AddRange(parts);
             this.SaveChanges();
         }
     }
---
+++ Darkspore.Server/src/model/CreaturePartModel.cs
@@ -0,0 +1,31 @@
+using HttpServer;
+
+namespace HttpServer;
+
+public class CreaturePartModel
+{
+    public ulong rigblockAssetId { get; set; }
+
+    public int cost { get; set; }
+    public int level { get; set; }
+    public int marketStatus { get; set; }
+    public int rarity { get; set; }
+    public int status { get; set; }
+    public int usage { get; set; }
+
+    public string stats { get; set; }
+    public string typeFull { get; set; }
+
+    public string classTypesFull { get; set; }
+    public string scienceTypesFull { get; set; }
+    public string rarityFull { get; set; }
+    public string pngKey { get; set; }
+
+    public int prefixAssetId { get; set; }
+    public int suffixAssetId { get; set; }
+    public int prefixSecondaryAssetId { get; set; }
+
+    public string weaponDamageModifier { get; set; }
+    public string modifiers { get; set; }
+    public string randSeed { get; set; }
+}
