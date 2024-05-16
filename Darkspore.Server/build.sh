a18ace9dc2dae7e68b5e8f4132581b73cb6ef347
VitorMM <vitor251093@gmail.com>
2024-05-16 01:04:27 -0300

    Updating build script

--- Darkspore.Server/build.sh
+++ Darkspore.Server/build.sh
@@ -1,4 +1,13 @@
-dotnet build --configuration Release --runtime win-x64
-dotnet build --configuration Release --runtime linux-x64
-dotnet build --configuration Release --runtime osx-x64
-dotnet build --configuration Release --runtime osx-arm64
+#!/bin/bash
+cd "$(dirname "$0")"
+
+# Cleaning prior builds
+sudo rm -rf bin/Release/
+
+# Windows build
+sudo dotnet build --configuration Release --runtime win-x64
+sudo cp -R ./resources ./bin/Release/net8.0/win-x64/resources
+
+#dotnet build --configuration Release --runtime linux-x64
+#dotnet build --configuration Release --runtime osx-x64
+#dotnet build --configuration Release --runtime osx-arm64
