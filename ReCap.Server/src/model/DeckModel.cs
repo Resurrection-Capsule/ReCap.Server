75ed233d3f8ec20480ec42f750df026b96f3c210
VitorMM <vitor251093@gmail.com>
2024-09-09 22:35:03 -0300

    Fixing typo

--- ReCap.Server/src/model/DeckModel.cs
+++ ReCap.Server/src/model/DeckModel.cs
@@ -11,6 +11,6 @@ public class DeckModel
 
     public required ulong AccountID { get; set; }
 
-    public required bool Locked { get; set; };
+    public required bool Locked { get; set; }
     public required List<ulong> CreatureIds { get; set; }
 }
