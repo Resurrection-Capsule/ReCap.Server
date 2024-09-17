61cea7a7eb08f542dc10d4cbb2cb69b7f96f0f7e
VitorMM <vitor251093@gmail.com>
2024-09-16 23:43:53 -0300

    Improving launcher build script

--- ReCap.Launcher/build.sh
+++ ReCap.Launcher/build.sh
@@ -6,7 +6,4 @@ sudo rm -rf bin/Release/
 
 # Windows build
 sudo dotnet build --configuration Release --runtime win-x64
-
-#dotnet build --configuration Release --runtime linux-x64
-#dotnet build --configuration Release --runtime osx-x64
-#dotnet build --configuration Release --runtime osx-arm64
+sudo chmod 777 -R bin/
