8ad323dad6f403ee0445e1ffd06bf05a3de5c9fe
VitorMM <vitor251093@gmail.com>
2024-09-16 23:44:22 -0300

    Improving server build script

--- ReCap.Server/build.sh
+++ ReCap.Server/build.sh
@@ -6,7 +6,7 @@ sudo rm -rf bin/Release/
 
 # Windows build
 sudo dotnet build --configuration Release --runtime win-x64
-sudo chmod 777 -R bin/Release/
+sudo chmod 777 -R bin/
 
 #dotnet build --configuration Release --runtime linux-x64
 #dotnet build --configuration Release --runtime osx-x64
